36398374b7e35bdd4a977cbc0e87614fea2ca598
Andrew Teixeira <teixeira@broadinstitute.org>
2021-01-07 14:47:58 -0500

    Make gcloud credentials available inside the container if available

--- terraform.sh
+++ terraform.sh
@@ -3,28 +3,26 @@
 DOCKER_IMAGE='broadinstitute/terraform:latest'
 SUDO=
 
-SCRIPT_DIR="$( cd -P "$( dirname "$BASH_SOURCE[0]" )" && pwd )"
+SCRIPT_DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=/dev/null
 source "${SCRIPT_DIR}/config.sh"
 
 usage() {
-    PROG=`basename $0`
+    PROG="$(basename "$0")"
     echo "usage: ${PROG} [--version] [--help] <command> [<args>]"
 }
 
-if [ "$TERM" != "dumb" ] ; then
+if [ "$TERM" != 'dumb' ] ; then
     TTY='-it'
 fi
 
-EXTRA_ENV=
-if [ -z "${ATLAS_TOKEN}" ]; then
-    echo "ATLAS_TOKEN has not been set."
-    exit 1
-else
-    EXTRA_ENV="-e ATLAS_TOKEN=${ATLAS_TOKEN}"
+EXTRA_OPTS=
+if [ -n "$ATLAS_TOKEN" ]; then
+    EXTRA_OPTS="-e ATLAS_TOKEN=${ATLAS_TOKEN}"
 fi
 
-if [ `uname -s` != "Darwin" ]; then
-    if [ ! -w "${DOCKER_SOCKET}" ]; then
+if [ "$(uname -s)" != 'Darwin' ]; then
+    if [ ! -w "$DOCKER_SOCKET" ]; then
         SUDO='sudo'
     fi
 fi
@@ -34,10 +32,18 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+# Map the terraform data directory into the container
 DATA_FQP="$( cd -P "${DATA_DIR}" && pwd )"
-if [ ! -d "${DATA_FQP}" ]; then
-    echo "Directory `${DATA_FQP}` does not exist...exiting."
+if [ ! -d "$DATA_FQP" ]; then
+    echo "Directory '${DATA_FQP}' does not exist...exiting."
     exit 2
 fi
 
-$SUDO docker run $TTY --rm -v $DATA_FQP:/data $EXTRA_ENV $DOCKER_IMAGE $@
+# Map gcloud configuration if it exists
+GCLOUD_CONFIG="$( cd -P "${HOME}/.config/gcloud" && pwd )"
+if [ -d "$GCLOUD_CONFIG" ]; then
+    EXTRA_OPTS="${EXTRA_OPTS} -v ${GCLOUD_CONFIG}:/root/.config/gcloud"
+fi
+
+# shellcheck disable=SC2068,SC2086
+$SUDO docker run $TTY --rm -v "${DATA_FQP}:/data" $EXTRA_OPTS "$DOCKER_IMAGE" $@
